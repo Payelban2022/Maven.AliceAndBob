90c14c87237ae7e9fea9cb46137e3476d464cb29
payel <payel.nandy2580@gmail.com>
2022-06-25 10:41:52 -0400

    Lab 2 Alice & Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        String name;
+        System.out.println("Please print your name.");
+        Scanner abc = new Scanner(System.in);
+        name = abc.nextLine();
+        name=name.toUpperCase();
+        if (name.equals("ALICE")) {
+            System.out.println("Hi Alice!");
+        } else if (name.equals("Bob")) {
+            System.out.println("Hi Bob!");
+        } else System.out.println("False");
 
     }
-}
+};
+
+
+
+
+
+
+
